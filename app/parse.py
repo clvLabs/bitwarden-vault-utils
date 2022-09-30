ca59cd910e54e91a7a46a2c6a76fb0cb3400c928
Tony Aguilar <onixag@gmail.com>
2022-09-30 23:29:06 +0200

    Change program output

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import time
 import src.bitwarden as bitwarden
 
 SAMPLE_FILE = "local/p.json"
@@ -21,16 +20,13 @@ def main():
   print(f"Found {len(vault.folders)} folders:")
   for folder in vault.folders.values():
     print()
-    print(f"- ---[{folder.name:20}: {len(folder.items):3}] {'-'*50}")
+    print(f"- ---[{len(folder.items):3} {folder.name:20}] {'-'*50}")
     for item in folder.items:
       _classname = bitwarden.helpers.get_item_class_name(item.type)
-      print(f"  - [{_classname:10}] {item.name:20}")
+      print(f"- [{item.id}] [{folder.name:20}] [{_classname:10}] {item.name}")
 
   print()
 
 
 if __name__ == "__main__":
-  start_time = time.time()
   main()
-  elapsed = time.time() - start_time
-  print(f"Finished in {elapsed:.2f} seconds")
