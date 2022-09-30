33601027b6924306b2fcfeaa7471637bd521023d
Tony Aguilar <onixag@gmail.com>
2022-09-30 23:15:03 +0200

    Add SecureNote fields

--- app/src/bitwarden/securenote.py
+++ app/src/bitwarden/securenote.py
@@ -6,6 +6,8 @@ class SecureNote(Item):
     def __init__(self):
       super().__init__()
 
+      self.note_type = None
+
 
     def __str__(self):
       return f"<bitwarden.SecureNote name=\"{self.name}\">"
@@ -17,5 +19,12 @@ class SecureNote(Item):
 
     @staticmethod
     def from_obj(obj):
+      if "secureNote" not in obj:
+        raise Exception("Item has no 'secureNote' field")
+
+      inner_obj = obj["secureNote"]
+
       new_obj = SecureNote()
+      new_obj._parse_item_fields(obj)
+      if "type" in inner_obj:    new_obj.note_type = inner_obj["type"]
       return new_obj
